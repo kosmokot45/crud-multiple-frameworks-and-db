4c2eaaba5a0bcc3d861b3a33d8c5454e5ab98852
pohofmd <kosmokot45@gmail.com>
2023-07-03 20:28:32 +0300

    add update func mongodb

--- mongo_crud_db.py
+++ mongo_crud_db.py
@@ -1,13 +1,25 @@
 ################## EXAMPLES
+
 ####### Create
+
 # new_show = {
 #     "name": "Vladimir",
 #     "age": 29
 # }
 # print(create_data(series_collection, new_show))
+
 ####### Read
+
 # result = read_data(series_collection, {'age': 25}, multiple=True)
 # print(result)
+
+####### Update
+
+# data = {'name': 'Vasiliy'}
+# data_info = read_data(series_collection, data)
+# id_ = data_info['_id']
+
+# update_data(series_collection, {'_id':id_}, {'name':'Narek'})
 
 from pymongo import MongoClient
 import settings
@@ -29,8 +41,8 @@ def read_data(collection, data, multiple=False):
     else:
         return collection.find_one(data)
 
-def update_data(collection, data):
-    pass
+def update_data(collection, query_data, updated_values):
+    collection.update_one(query_data, {'$set': updated_values})
 
 def delete_data(collection, data):
     pass
